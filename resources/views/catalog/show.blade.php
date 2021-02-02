fd4fee17cca499e889f21ce4e88778679f27ddb4
jessica <jesi7gmar@gmail.com>
2021-02-02 11:23:20 +0100

    02/022021

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -5,18 +5,8 @@
 <div class="row">
 
     <div class="col-sm-4">
-        @foreach( $arrayPeliculas as $key) <!--Notación de blade-->
-                @foreach ($key as $pelicula)
-                {{$pelicula['title']}}
-                @endforeach
-    <div class="col-xs-6 col-sm-4 col-md-3 text-center">
 
-         <!--Para que al pulzar sobre la pelicula nos lleve a más info de la peli-->
-         
-    </div>
-    @endforeach
-        
-        hola mundo
+        {{-- TODO: Imagen de la película --}}
 
     </div>
     <div class="col-sm-8">
